5a07e9f8b8bd37a09370c474b75453e848d0b1c5
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-10 22:33:04 -0500

    added more code to js and schemas files

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const ( put typeDefs, placesToGo, trips, users ) = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+
 
 const Port = process.env.Port || 3001;
 const app = express();
 const server = new ApolloServer({
-    put schemas names Here,
+   typeDefs,
+   resolvers,
 });
 
 app.use(express.urlencoded({ extended: false}));
