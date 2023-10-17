ece96ea79512c7c49c57036ab5d2ca53242937d3
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-17 18:18:05 -0500

    worked on logout function

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Trips } = require('../models');
 const { signToken } = require('../utils/auth');
-const { getTravelAdvisorData } = require('./');
+const { getTravelAdvisorData } = require('../schemas');
 
 const resolvers = {
   Query: {
