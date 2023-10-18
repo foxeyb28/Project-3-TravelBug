5872323650749be9a2663533b12e8d110861fff9
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-18 18:42:18 -0500

    worked on home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,19 +1,46 @@
-import React, { useEffect } from 'react';
-import M from 'materialize-css';
+import React, { useEffect, useState } from 'react';
+import M from 'materialize-css'; 
+import auth from '../../utils/auth';
+import TripForm from '../TripForm'; 
 
-export default function Home({ handleChange, handleSearch, cityName }) {
+export default function Home({ handleChange, handleSearch, cityName, user }) {
+  const [userData, setUserData] = useState(null);
+
   useEffect(() => {
     M.AutoInit();
+    try {
+      console.log(auth.getProfile());
+    } catch (e) {
+      console.error("OH NO", e);
+    }
+
   }, []);
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col s12">
+  const renderPersonalizedContent = () => {
+    if (userData) {
+      return (
+        <div>
+          <h1>Welcome, {userData.username}!</h1>
+          {userData.preferences === 'traveler' ? (
+            <p>Explore amazing destinations</p>
+          ) : (
+            <p>Discover local gems near you</p>
+          )}
+        </div>
+      );
+    } else {
+      return (
+        <div>
           <h1>Welcome to Travel Bug</h1>
           <p>Your Adventure Awaits</p>
         </div>
-      </div>
+      );
+    }
+  };
+
+  return (
+    <div className="container">
+      {renderPersonalizedContent()}
 
       <div className="row">
         <div className="input-field col s12">
@@ -40,9 +67,9 @@ export default function Home({ handleChange, handleSearch, cityName }) {
             Search
             <i className="material-icons right"></i>
           </button>
-          
         </div>
       </div>
+      <TripForm /> 
     </div>
   );
 }
