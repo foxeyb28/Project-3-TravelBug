faee73b64e29299b1bea667698df6cf4d7bf239c
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-18 23:34:35 -0500

    added to mytrips

--- client/src/components/pages/MyTrips.js
+++ client/src/components/pages/MyTrips.js
@@ -1,11 +1,21 @@
-import React from "react";
+iimport React from "react";
 import MyCardComponent from "./Card"; // Import your card component
 import 'materialize-css/dist/css/materialize.min.css'; 
+import React, { useRef } from 'react';
+import AttractionsCard from "../AttractionsCard";
+import RestaurantCard from "../RestaurantCard";
 
-function MyTrips({ trips }) {
+function MyTrips() {
+  const inputRef = useRef("MyTrips");
+  const handleSubmit = () => {
+    console.log(inputRef.current.value);
+  };
+
   return (
     <div>
       <h1>My Trips</h1>
+      <input type="text" ref={inputRef} />
+      <button onClick={handleSubmit}>Submit</button>
       {trips.map((trip) => (
         <MyCardComponent key={trip.id} trip={trip} />
       ))}
