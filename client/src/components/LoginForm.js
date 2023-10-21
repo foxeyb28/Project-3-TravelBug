041dc008aabe13f2df6848034bcd4e286b7891a6
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-20 20:58:20 -0500

    worked on the resolvers

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -26,7 +26,7 @@ const LoginForm = () => {
       console.log(data);
       Auth.login(data.loginUser.token);
     } catch (err) {
-      console.error(err);
+     // console.error(err);
       setShowAlert(true);
     }
 
